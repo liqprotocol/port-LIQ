6d0380a41b90d7dfac5dd0503f923d64d5f2daf5
Wang Ge <gw980531@gmail.com>
2021-06-10 13:17:38 +0800

    Added helper function.

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,10 @@
 import { Connection, PublicKey } from '@solana/web3.js';
 import axios from 'axios';
 import { Obligation, ObligationParser } from './layouts/obligation';
+import { bits, blob, struct, u8, u32, nu64 } from 'buffer-layout';
 import { EnrichedReserve, ReserveParser } from './layouts/reserve';
 
+export const TOKEN_PROGRAM_ID = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
 
 export function notify(content) {
   if (process.env.WEBHOOK_URL) {
@@ -73,4 +75,49 @@ export async function getParsedReservesMap(connection: Connection, programId: Pu
     )
   }
   return parsedReserves;
+}
+
+export async function findLargestTokenAccountForOwner(
+  connection: Connection,
+  owner: PublicKey,
+  mint: PublicKey
+): Promise<{ publicKey: PublicKey; tokenAccount: { mint: PublicKey; owner: PublicKey; amount: number} }> {
+
+  const response = await connection.getTokenAccountsByOwner(owner, {mint}, connection.commitment)
+  let max = -1;
+  let maxTokenAccount: null | { mint: PublicKey; owner: PublicKey; amount: number} = null
+  let maxPubkey: null | PublicKey = null
+  for (const { pubkey, account } of response.value) {
+
+    const tokenAccount = parseTokenAccountData(account.data)
+    if (tokenAccount.amount > max) {
+      maxTokenAccount = tokenAccount
+      max = tokenAccount.amount
+      maxPubkey = pubkey
+    }
+  }
+
+  if (maxPubkey && maxTokenAccount) {
+    return {publicKey: maxPubkey, tokenAccount: maxTokenAccount}
+  } else {
+    throw new Error("No accounts for this token")
+  }
+}
+
+export const ACCOUNT_LAYOUT = struct([
+  blob(32, 'mint'),
+  blob(32, 'owner'),
+  nu64('amount'),
+  blob(93)
+]);
+
+export function parseTokenAccountData(
+  data: Buffer,
+): { mint: PublicKey; owner: PublicKey; amount: number } {
+  let { mint, owner, amount } = ACCOUNT_LAYOUT.decode(data);
+  return {
+    mint: new PublicKey(mint),
+    owner: new PublicKey(owner),
+    amount,
+  };
 }
