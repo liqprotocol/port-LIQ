8b77af5d0d7d454e0928234126f10b60b874717a
Linus Kendall <git@linuskendall.com>
2022-01-24 13:30:33 +0530

    Use the correct connection object (#44)
    Use the correct connection object to connect to the right clusterUrl for the liquidations.

--- src/liquidator.ts
+++ src/liquidator.ts
@@ -78,7 +78,7 @@ async function runLiquidator() {
 
   console.log(`Port liquidator launched on cluster=${clusterUrl}`);
 
-  const reserveContext = await Port.forMainNet({}).getReserveContext();
+  const reserveContext = await Port.forMainNet({connection: connection}).getReserveContext();
 
   const wallets = await prepareTokenAccounts(provider, reserveContext);
 
